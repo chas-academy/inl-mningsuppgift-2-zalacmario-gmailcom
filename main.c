4cd72d2cbfca6a37c5cd7cf4826ace3e24ef1fb5
ZaCc42 <zalac.mario1@gmail.com>
2025-05-27 11:35:14 +0200

    first

--- main.c
+++ main.c
@@ -2,6 +2,17 @@
 
 
 // This is where you add the two functions
+float fahrenheit_to_celsius(float f){
+    float c = (f - 32) * 5.0 / 9.0;
+    return c;
+
+}
+
+
+float celsius_to_fahrenheit(float c){
+    float f = (c * 9.0) / 5.0 + 32;
+    return f;
+}
 
 int main() {
     float temp_f = 100.0;
